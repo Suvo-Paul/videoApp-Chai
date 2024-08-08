1cc6f7e0e5368384b77c8b27bdc8333fdaadbf33
Suvo-Paul <suvopaul098@gmail.com>
2024-08-08 22:52:58 +0530

    changes

---
+++ src/db/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connection = await mongoose.connect(`${process.env.MONGODB_URL}/ ${DB_NAME}`)
+        console.log(`Database is connected ${connection.connection.host}`);
+
+    } catch (error) {
+        console.log("Database connection failed", error.message);
+        process.exit(1)
+    }
+}
+
+export default connectDB;
