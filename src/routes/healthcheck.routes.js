20e96644fdaaa006c3ec0e2a39c8f2e488881806
Suvo-Paul <suvopaul098@gmail.com>
2024-09-23 20:28:06 +0530

    changes

--- src/routes/healthcheck.routes.js
+++ src/routes/healthcheck.routes.js
@@ -3,6 +3,6 @@ import { healthcheck } from "../controllers/healthcheck.controllers.js"
 
 const router = Router();
 
-router.route('/').get(healthcheck);
+router.route('/test').get(healthcheck);
 
 export default router
